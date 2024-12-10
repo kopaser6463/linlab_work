75352a0ea25cb009b71b5342930cff3c18bc7a0b
Lebedev_leonid <LLS.04@yandex.ru>
2024-12-10 15:49:55 +0300

    feat: get fields and csr to std format

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 class CSR_format{
 private:
@@ -9,15 +10,15 @@ private:
 	std::vector<int>col;
 	std::vector<float>value;
 public:
-	CSR_format(std::vector<std::vector<float>> matrix_nformat){
-		this->n = matrix_nformat.size();
-		this->m = matrix_nformat[0].size();
+	CSR_format(std::vector<std::vector<float>> matrix_std){
+		this->n = matrix_std.size();
+		this->m = matrix_std[0].size();
 		for (int i = 0; i < n; i++){
 			for (int j = 0; j < m; j++){
-				if (matrix_nformat[i][j] != 0){
+				if (matrix_std[i][j] != 0){
 					this->row.push_back(i);
 					this->col.push_back(j);
-					this->value.push_back(matrix_nformat[i][j]);
+					this->value.push_back(matrix_std[i][j]);
 				}
 			}
 		}
@@ -31,6 +32,34 @@ public:
 			}
 		}
 		return res;
+	}
+	
+	short get_n(){
+		return this->n;
+	}
+
+	short get_m(){
+		return this->m;
+	}
+
+	std::vector<int> get_row(){
+		return this->row;
+	}
+
+	std::vector<int> get_col(){
+		return this->col;
+	}
+
+	std::vector<float> get_value(){
+		return this->value;
+	}
+
+	std::vector<std::vector<float>> csr_to_std(){
+		std::vector<std::vector<float>> matrix (n, std::vector<float> (m, 0));
+		for (int i = 0; i < value.size(); i++){
+			matrix[row[i]][col[i]] = value[i];
+		}
+		return matrix;
 	}
 };
 
