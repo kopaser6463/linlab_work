5ecfd92cfb61363cba49bc9ee5d10138adb77fb3
kopaser <kopaser>
2024-12-13 10:51:38 +0300

    feat: fix bugs, and made it working?

--- main.cpp
+++ main.cpp
@@ -66,31 +66,53 @@ public:
 int test(){
 	std::vector<std::string> a;
 	std::string num;
-	std::string read; 
-	std::cin >> num;
+	std::string numb;
+	std::string read;
+	std::string line_input;
+		
+	std::getline(std::cin, num, ' ');
+	std::getline(std::cin, numb);
+	//std::getline(std::cin, line_input);
+	
+	//std::stringstream ss(line_input);
+	//std::getline(ss, num, ' ');
+	//std::getline(ss, numb);
+	
+	//std::cin >> num;
+	//std::cout << "s" << num << "s" << " " << "s" << numb << "s" << "\n";
 	std::string buffer = "";
-	for (int i = 0; i <= std::stoi(num); i++){
+	for (int i = 0; i < std::stoi(num); i++){
 		std::getline(std::cin, read);
+		read += ' ';
+		//std::cout << "s" << read << "s" << "\n";
 		buffer = "";
 		for (int j = 0; j <= read.length(); j ++){
 			if (read[j] != ' '){
 				buffer += read[j];	
 			}
 			else{
+				if (buffer.length() != 0){
 				a.push_back(buffer);	
 				buffer = "";
+				}
 			}
 		}
-		a.push_back(buffer);
 	}
+	/*
+	for(std::string i: a){
+		std::cout << "s" << i << "s" << "\n";
+	}
+	*/
 	int numm = std::stoi(num);
+	int numbm = a.size()/numm;
 	//std::cout << a.size() / numm << "\n";
-	std::vector<std::vector<float>> array(numm, std::vector<float> (a.size() / numm));
-	
-	
-	for (int i = 1; i < a.size(); i++){
-		//std::cout << "s" <<a[i]<< "s" << "\n";
-		array[(i-1)%numm][(i-1)/(a.size() / numm)] = std::stof(a[i]);
+	std::vector<std::vector<float>> array(numbm, std::vector<float> (numm));
+	//std::cout << 2 % (numm+1) << "\n";
+	//std::cout << numm << "\n";
+	//std::cout << a.size() << "\n";
+	for (int i = 0; i < a.size(); i++){
+		//std::cout << "s" << (i)/(numm) << "s" << " " << "s" <<a[i]<< "s" << " " << "s" << (i) % numm << "s" << "\n" ;
+		array[(i)/(numm)][(i) % numm] = (float)std::stoi(a[i]);
 	}
 	/*
 	for(std::vector<float> i: mat){
