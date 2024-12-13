932f8564b2a34612b9448d1791ad09e42e9c4999
Lebedev_leonid <LLS.04@yandex.ru>
2024-12-13 15:26:15 +0300

    feat: changing the architecture

--- first_task.cpp
+++ first_task.cpp
@@ -1,75 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <map>
+#include <CSR_format.cpp>
 
-class CSR_format{
-private:
-	short n;
-	short m;
-	std::vector<int>row;
-	std::vector<int>col;
-	std::vector<float>value;
-public:
-	CSR_format(std::vector<std::vector<float>> matrix_std){
-		this->n = matrix_std.size();
-		this->m = matrix_std[0].size();
-		for (int i = 0; i < n; i++){
-			for (int j = 0; j < m; j++){
-				if (matrix_std[i][j] != 0){
-					this->row.push_back(i);
-					this->col.push_back(j);
-					this->value.push_back(matrix_std[i][j]);
-				}
-			}
-		}
-	}
-
-	float trace(){
+	float trace(CSR_format mat){
 		float res = 0;
-		for (int i = 0; i < value.size(); i++){
-			if (row[i] == col[i]){
-				res += value[i];
+		for (int i = 0; i < mat.value.size(); i++){
+			if (mat.row[i] == mat.col[i]){
+				res += mat.value[i];
 			}
 		}
 		return res;
 	}
-	
-	short get_n(){
-		return this->n;
-	}
 
-	short get_m(){
-		return this->m;
-	}
-
-	std::vector<int> get_row(){
-		return this->row;
-	}
-
-	std::vector<int> get_col(){
-		return this->col;
-	}
-
-	std::vector<float> get_value(){
-		return this->value;
-	}
-
-	std::vector<std::vector<float>> csr_to_std(){
-		std::vector<std::vector<float>> matrix (n, std::vector<float> (m, 0));
-		for (int i = 0; i < value.size(); i++){
-			matrix[row[i]][col[i]] = value[i];
-		}
-		return matrix;
-	}
-
-	void matrix_const_multiply(const float number){
-		for (int i = 0; i < value.size(); i++){
-			value[i] *=  number;
+	void matrix_const_multiply(const float number, CSR_format mat){
+		for (int i = 0; i < mat.value.size(); i++){
+			mat.value[i] *=  number;
 		}
 	}
-
-
-};
 
 int test(){
 	std::vector<std::string> a;
@@ -131,35 +76,35 @@ int test(){
 	*/
 	CSR_format mat(array);
 	std::cout << "value: ";
-	for (float i: mat.get_value()){
+	for (float i: mat.value){
 		std::cout << i << " ";
 	}
 	std::cout << "\n";
 	std::cout << "row: ";
-	for (float i: mat.get_row()){
+	for (float i: mat.row){
 		std::cout << i << " ";
 	}
 	std::cout << "\n";
 	std::cout << "col: ";
-	for (float i: mat.get_col()){
+	for (float i: mat.col){
 		std::cout << i << " ";
 	}
 	std::cout << "\n";
 	
-	mat.matrix_const_multiply(2);
+	matrix_const_multiply(2, mat);
 	
 	std::cout << "value: ";
-	for (float i: mat.get_value()){
+	for (float i: mat.value){
 		std::cout << i << " ";
 	}
 	std::cout << "\n";
 	std::cout << "row: ";
-	for (float i: mat.get_row()){
+	for (float i: mat.row){
 		std::cout << i << " ";
 	}
 	std::cout << "\n";
 	std::cout << "col: ";
-	for (float i: mat.get_col()){
+	for (float i: mat.col){
 		std::cout << i << " ";
 	}
 	std::cout << "\n";
