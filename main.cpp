dea050006cf0ab6a51d0a281667536291e81e845
kopaser <kopaser>
2024-12-13 14:02:28 +0300

    feat: удалил закоменченные линии

--- main.cpp
+++ main.cpp
@@ -72,19 +72,11 @@ int test(){
 		
 	std::getline(std::cin, num, ' ');
 	std::getline(std::cin, numb);
-	//std::getline(std::cin, line_input);
-	
-	//std::stringstream ss(line_input);
-	//std::getline(ss, num, ' ');
-	//std::getline(ss, numb);
-	
-	//std::cin >> num;
-	//std::cout << "s" << num << "s" << " " << "s" << numb << "s" << "\n";
+
 	std::string buffer = "";
 	for (int i = 0; i < std::stoi(num); i++){
 		std::getline(std::cin, read);
 		read += ' ';
-		//std::cout << "s" << read << "s" << "\n";
 		buffer = "";
 		for (int j = 0; j <= read.length(); j ++){
 			if (read[j] != ' '){
@@ -98,29 +90,16 @@ int test(){
 			}
 		}
 	}
-	/*
-	for(std::string i: a){
-		std::cout << "s" << i << "s" << "\n";
-	}
-	*/
+
 	int numm = std::stoi(num);
 	int numbm = a.size()/numm;
-	//std::cout << a.size() / numm << "\n";
+
 	std::vector<std::vector<float>> array(numbm, std::vector<float> (numm));
-	//std::cout << 2 % (numm+1) << "\n";
-	//std::cout << numm << "\n";
-	//std::cout << a.size() << "\n";
+
 	for (int i = 0; i < a.size(); i++){
-		//std::cout << "s" << (i)/(numm) << "s" << " " << "s" <<a[i]<< "s" << " " << "s" << (i) % numm << "s" << "\n" ;
 		array[(i)/(numm)][(i) % numm] = (float)std::stoi(a[i]);
 	}
-	/*
-	for(std::vector<float> i: mat){
-		for(float j : i){
-			std::cout << j << "\n";
-		}
-	}
-	*/
+
 	CSR_format mat(array);
 	std::cout << "value: ";
 	for (float i: mat.get_value()){
