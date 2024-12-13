9cfe32969c8e6059a31d936953b655090026981a
Lebedev_leonid <LLS.04@yandex.ru>
2024-12-13 17:25:05 +0300

    feat: перенос класса и подсчета следа

--- first_task.cpp
+++ first_task.cpp
@@ -1,16 +1,46 @@
-#include "CSR_format.cpp"
+#include <iostream>
+#include <vector>
 
-	float trace(CSR_format mat){
+class CSR_format{
+public:
+	short n;
+	short m;
+	std::vector<int>row;
+	std::vector<int>col;
+	std::vector<float>value;
+
+	CSR_format(std::vector<std::vector<float>> matrix_std){
+		this->n = matrix_std.size();
+		this->m = matrix_std[0].size();
+		for (int i = 0; i < n; i++){
+			for (int j = 0; j < m; j++){
+				if (matrix_std[i][j] != 0){
+					this->row.push_back(i);
+					this->col.push_back(j);
+					this->value.push_back(matrix_std[i][j]);
+				}
+			}
+		}
+	}
+
+	float trace(){
 		float res = 0;
-		for (int i = 0; i < mat.value.size(); i++){
-			if (mat.row[i] == mat.col[i]){
-				res += mat.value[i];
+		for (int i = 0; i < value.size(); i++){
+			if (row[i] == col[i]){
+				res += value[i];
 			}
 		}
 		return res;
 	}
 
-//simple
+	std::vector<std::vector<float>> csr_to_std(){
+		std::vector<std::vector<float>> matrix (n, std::vector<float> (m, 0));
+		for (int i = 0; i < value.size(); i++){
+			matrix[row[i]][col[i]] = value[i];
+		}
+		return matrix;
+	}
+};
 
 int main(){
 	return 0;
